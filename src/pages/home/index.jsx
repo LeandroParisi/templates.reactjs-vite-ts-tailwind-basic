977790b7b3a94db49f99eccc6239953fb88e550f
LeandroParisi <lepcbelisario@gmail.com>
2024-06-13 08:53:13 +0200

    wip

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,5 @@
 import { DashboardLayout } from 'layouts/dashboard-layout'
-import { useExampleService } from 'services/use-example-service'
 
 export function Home() {
-  const { query } = useExampleService()
   return <DashboardLayout>Home</DashboardLayout>
 }
